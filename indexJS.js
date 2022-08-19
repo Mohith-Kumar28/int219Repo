389a13f1a8fdae9aeceabfb4a18d1bea49dbe19a
Mohith kumar <murugan28aug@gmail.com>
2022-08-19 10:51:08 +0530

    js

---
+++ indexJS.js
@@ -0,0 +1,24 @@
+let myvar = "hello  welcome";
+        console.log("hello 36487834793");
+        console.log(typeof "hello");
+        console.log(typeof true);
+        console.log(myvar.length);
+        console.log(myvar.charAt(9));
+        console.log(myvar.indexOf("l"));
+        console.log(myvar.slice(5,18));
+        console.log(myvar.toUpperCase());
+        console.log(myvar.toLowerCase());
+        console.log(myvar.includes("worlds,kjsavb"));
+        console.log(myvar.charAt(myvar.length-1));  
+        console.log(myvar.slice(-1));
+        var a = prompt("Enter a number for a innnn");
+        var b = prompt("Enter a number for bsth");
+        if((a<b))
+        document.write("a is < than b");
+        if((a>b))
+        document.write("a is greater than b");
+        else
+        document.write("equals");
+        for(var i=0; i<10;i++){
+            document.write("<br>"+i );
+        }
